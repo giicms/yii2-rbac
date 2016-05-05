416f8fdc6af0f6e5642d087d4be2fbdd5cbb4b3a
Giicms <giicms@gmail.com>
2016-05-05 15:53:27 +0700

    Update code

---
+++ controllers/PermissionController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace giicms\rbac\controllers;
+
+use giicms\rbac\controllers\ItemController;
+use yii\rbac\Item;
+
+class PermissionController extends ItemController
+{
+
+    /**
+     * @inheritdoc
+     */
+    public function labels()
+    {
+        return[
+            'Item' => 'Permission',
+            'Items' => 'Permissions',
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getType()
+    {
+        return Item::TYPE_PERMISSION;
+    }
+
+}
---
+++ controllers/RoleController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace giicms\rbac\controllers;
+
+use giicms\rbac\controllers\ItemController;
+use yii\rbac\Item;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class RoleController extends ItemController
+{
+
+    /**
+     * @inheritdoc
+     */
+    public function labels()
+    {
+        return[
+            'Item' => 'Role',
+            'Items' => 'Roles',
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getType()
+    {
+        return Item::TYPE_ROLE;
+    }
+
+}
